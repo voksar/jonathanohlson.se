e9c459053875825ba610c100215e6b7ac682bfb2
voksar <jo@jonathanohlson.se>
2021-03-10 00:27:45 +0100

    added back everything except db query on startup

--- server/app/config.py
+++ server/app/config.py
@@ -9,8 +9,8 @@ class ConfigProduction(object):
     DEVELOPMENT = False
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@192.168.1.150:3306/JONWEBDB'.format(os.environ.get('DB_USERNAME'), os.environ.get('DB_PASSWORD'))
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@192.168.1.150:3306/JONWEBDB'.format(os.environ.get('DB_USERNAME'), os.environ.get('DB_PASSWORD'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     
     #JWT Variables
@@ -23,8 +23,8 @@ class ConfigDevelopment(object):
     DEVELOPMENT = True
     DEBUG = True
     SECRET_KEY = "DEV123"
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://0:0@192.168.1.150:3306/JONWEBDB_TEST'
-    #SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://0:0@192.168.1.150:3306/JONWEBDB_TEST'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = 'Content-Type'
 
     #JWT Variables
