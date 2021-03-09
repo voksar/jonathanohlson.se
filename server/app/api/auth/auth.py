71896bf47ef27eaa767b67ce05374a56f348212f
Jonathan <63061970+voksar@users.noreply.github.com>
2021-03-09 23:44:51 +0100

    Revert "Dev"

--- server/app/api/auth/auth.py
+++ server/app/api/auth/auth.py
@@ -16,7 +16,6 @@ from models.user import User
 
 from utils.role_required import admin_required
 
-import os
 
 
 auth = Blueprint('auth', __name__, url_prefix='/api/auth')
@@ -70,7 +69,3 @@ def check_valid():
 def check_admin(*args, **kwargs):
     
     return {'msg': 'User is admin!', 'admin': True}, 200
-
-@auth.route('/test')
-def test():
-    return {'msg':os.environ.get('FLASK_ENV')}
