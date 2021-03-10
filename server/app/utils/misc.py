45b275bb69a696afb69e8f92c362b5e6111f8c2e
voksar <jo@jonathanohlson.se>
2021-03-10 09:11:53 +0100

    remove json_key and import for it

--- server/app/utils/misc.py
+++ server/app/utils/misc.py
@@ -1,8 +1,2 @@
 import json
 import os
-
-
-def Json_Key():
-    with open(os.getcwd() + "/config.json", "r") as json_file:
-        json_data = json.load(json_file)
-    return str(json_data['SECRET_KEY'])
