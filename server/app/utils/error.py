ccdf38f7278562f4e4f583daa686b07baaeb3fab
voksar <jo@jonathanohlson.se>
2021-03-15 23:13:29 +0100

    changed minor stuff

--- server/app/utils/error.py
+++ server/app/utils/error.py
@@ -2,7 +2,6 @@ class Error(Exception):
     pass
 
 class UserCantBeDeleted(Error):
-
     def __init__(self, message='User cant be deleted', code=403):
         self.msg = message
         self.code = code
