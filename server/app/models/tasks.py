77db79b5bb96de2b36f810acb71f3b9eee20112d
voksar <jo@jonathanohlson.se>
2021-03-24 22:04:41 +0100

    added major revamp in API and Errorhandling

--- server/app/models/tasks.py
+++ server/app/models/tasks.py
@@ -1,9 +1,13 @@
 from . import db
 from datetime import datetime
 
+from flask import jsonify
+
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import get_jwt
+
+from sqlalchemy.exc import SQLAlchemyError
 
 import models.user as user
 
@@ -21,28 +25,43 @@ class Tasks(db.Model):
 
     @staticmethod
     def get_tasks(id):
-        tasks = Tasks.query.filter_by(user_id=id)
-        if tasks:
-            return tasks
-        else: 
-            return None
+        try:
+            tasks = Tasks.query.filter_by(user_id=id)
+            list_of_tasks = []
+            for task in tasks:
+                dictionary = {'id': task.id, 'text': task.text, 'created': task.created}
+                list_of_tasks.append(dictionary)
+            
+            return jsonify(list_of_tasks), 200
+        except SQLAlchemyError as e:
+            return {'msg': e._message}, 500
     
     @staticmethod
     def delete_task(task_id):
-        task = db.session.query(Tasks).filter(Tasks.id==task_id).first()
-        db.session.delete(task)
-        db.session.commit()
+        
+        try:
+            task = db.session.query(Tasks).filter(Tasks.id==task_id).first()
+            db.session.delete(task)
+            db.session.commit()
+            resp = {'msg': 'Task deleted successfully'}, 200
+        except SQLAlchemyError as e:
+            resp = { 'msg': e._message }, 500
+        
+        return resp
     
     @staticmethod
     def add_task(text, username):
-        _user = db.session.query(user.User).filter(user.User.username==username).first()
+        try:
+            _user = db.session.query(user.User).filter(user.User.username==username).first()
 
-        task = Tasks(text=text, created=datetime.now(), user_id=_user.id)
+            task = Tasks(text=text, created=datetime.now(), user_id=_user.id)
 
-        db.session.add(task)
-        db.session.flush()
-        resp = {'id': task.id, 'created':task.created}
+            db.session.add(task)
+            db.session.flush()
+            resp = {'id': task.id, 'created': task.created}, 200
 
-        db.session.commit()
+            db.session.commit()
+        except SQLAlchemyError as e:
+            resp =  {'msg': e._message}, 500
         
         return resp
